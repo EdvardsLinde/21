1380f2703cbda25426d5b7606f67a4af70e19e9e
EdvardsLinde <edvards.linde@gmail.com>
2024-09-30 22:19:52 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+# def sum(a, b):
+#     return (a + b)
+
+# a = int(input('Enter 1st number: '))
+# b = int(input('Enter 2nd number: '))
+
+# print(f'Sum of {a} and {b} is {sum(a, b)}')
+
+
+def sum(a):
+    return (2024 - a)
+  
+  
+a = int(input("Type your age"))
+
+print(f'Your birth year is {2024 - a}')
+  
+def (2024 - a}:
+      return list (a)
+
+print(a)
